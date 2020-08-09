b988457e9115d33b5380a80557bd2caeba276769
hectorm <hmhernandez2001@gmail.com>
2020-08-09 01:52:19 -0400

    adding controllers and pages

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinicapp/controllers/VetController.java
@@ -0,0 +1,16 @@
+package guru.springframework.petclinicapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping(value = {"","/","/index", "/index.html"}, method = RequestMethod.GET)
+    public String listVets(){
+        return "vets/index";
+    }
+
+}
